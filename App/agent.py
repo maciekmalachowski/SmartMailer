cc32ad7767c59cff2572d3aed5b4eab7caf08e70
maciekmalachowski <maciejmalachowski1@gmail.com>
2024-12-03 19:07:12 +0100

    Media add

--- App/agent.py
+++ App/agent.py
@@ -5,7 +5,7 @@ from llama_index.experimental.query_engine import PandasQueryEngine
 from llama_index.core.tools import QueryEngineTool, ToolMetadata
 from llama_index.core.agent import ReActAgent
 from llama_index.core.agent.react import ReActChatFormatter
-from prompts import instruction_str, new_prompt, context
+from prompts import instruction_str, new_prompt, context, start_message, quit_message
 from mailing import send_email_engine
 from data_manipulations import add_record_engine, delete_record_engine, update_email_engine, change_if_sent_engine
 from data_manager import company_df
@@ -15,7 +15,7 @@ load_dotenv()
 
 # Initialize the LLMs from Ollama
 llm = Ollama(model="llama3.1", request_timeout=60.0)
-agent_llm = OpenAI(model="gpt-4o-mini", request_timeout=60.0)
+agent_llm = OpenAI(model="gpt-4o", request_timeout=60.0)
 
 # Create a PandasQueryEngine for querying the company DataFrame using the given mistrl LLM
 company_query_engine = PandasQueryEngine(df=company_df, verbose=False, instruction_str=instruction_str, llm=llm)
@@ -41,12 +41,14 @@ tools = [
 agent = ReActAgent.from_tools(
     tools=tools,
     llm=agent_llm,
-    verbose=True,
+    verbose=False,
     max_iterations=15,
     react_chat_formatter=ReActChatFormatter.from_defaults(context=context)
 )
 
 # The user inputs a prompt, and the agent provides a result based on the tools available
+print(start_message)
 while (prompt := input("Enter a prompt (q to quit): ")) != "q":
     result = agent.chat(prompt)
-    print(result)
+    print(f"\n-----------------\n{result}\n-----------------\n")
+print(quit_message)
